4623158d0d561e07b646700484c9126553c78484
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-04-12 01:53:08 +0300

    Configures the react router for the the user and home pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import './app.css'
 import Topbar from './components/topbar/Topbar'
 import Sidebar from './components/siderbar/Sidebar'
 import Home from './pages/home/Home'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import UserList from './pages/userList/UserList';
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Topbar/>
       <div className="container">
         <Sidebar/>
-        <Home/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/users" element={<UserList/>}/>
+        </Routes>
       </div>
-    </div>
+    </Router>
   )
 }
 
