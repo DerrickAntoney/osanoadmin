667a19f0b75bd2a4900390b071427ec86783d310
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-04-13 03:13:35 +0300

    Finishes update widget styling and configurations.

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,19 +1,22 @@
-import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid } from '@mui/icons-material'
+import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid, Publish } from '@mui/icons-material'
 import './user.css'
+import { Link } from 'react-router-dom'
 
 const User = () => {
   return (
     <div className='user'>
         <div className="userTitleContainer">
             <h1 className='userTitle'>Edit User</h1>
-            <button className='userAddButton'>Create</button>
+            <Link to='/newUser'>
+                <button className='userAddButton'>Create</button>
+            </Link>
         </div>
         <div className="userContainer">
             <div className="userShow">
                 <div className="userShowTop">
                     <img className='userShowImg' src="../../../public/assets/avatar.jpg" alt="" />
                     <div className="userShowTopTitle">
-                        <span className="userShowUsername">Anna Becker</span>
+                        <span className="userShowUsername">Fred Becker</span>
                         <span className="userShowTitle">Software Engineer</span>
                     </div>
                 </div>
@@ -21,7 +24,7 @@ const User = () => {
                     <span className="userShowTitle">Account Details</span>
                     <div className="userShowInfo">
                         <PermIdentity className='userShowIcon'/>
-                        <span className="userShowInfoTitle">annabeck99</span>
+                        <span className="userShowInfoTitle">fredbeck99</span>
                     </div>
                     <div className="userShowInfo">
                         <CalendarToday className='userShowIcon'/>
@@ -34,16 +37,64 @@ const User = () => {
                     </div>
                     <div className="userShowInfo">
                         <MailOutline className='userShowIcon'/>
-                        <span className="userShowInfoTitle">annabeck99@gmail.com</span>
+                        <span className="userShowInfoTitle">fredbeck99@gmail.com</span>
                     </div>
                     <div className="userShowInfo">
                         <LocationSearching className='userShowIcon'/>
                         <span className="userShowInfoTitle">Nairobi | Kenya</span>
                     </div>
                 </div>
-                
             </div>
-            <div className="userUpdate"></div>
+            <div className="userUpdate">
+                <span className="userUpdateTitle">Edit</span>
+                <form className='userUpdateForm'>
+                        <div className="userUpdateLeft">
+                            <div className="userUpdateItem">
+                                <label>Username</label>
+                                <input 
+                                    className='userUpdateInput' 
+                                    type="text"
+                                    placeholder='fredbeck99'/>
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Full Name</label>
+                                <input 
+                                    className='userUpdateInput' 
+                                    type="text"
+                                    placeholder='Fred Becker'/>
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Email</label>
+                                <input 
+                                    className='userUpdateInput' 
+                                    type="text"
+                                    placeholder='fredbeck99@gmail.com'/>
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Phone</label>
+                                <input 
+                                    className='userUpdateInput' 
+                                    type="text"
+                                    placeholder='+254 768 444 443'/>
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Address</label>
+                                <input 
+                                    className='userUpdateInput' 
+                                    type="text"
+                                    placeholder='Nairobi | Kenya'/>
+                            </div>
+                        </div>
+                        <div className="userUpdateRight">
+                            <div className="userUpdateUpload">
+                                <img src="../../../public/assets/avatar.jpg" alt="" className='userUpdateImg'/>
+                                <label htmlFor="file"><Publish className='userUpdateIcon'/></label>
+                                <input type="file" id="file" style={{display:"none"}}/>
+                            </div>
+                            <button className="userUpdateButton">Update</button>
+                        </div>
+                    </form>
+            </div>
         </div>
     </div>
   )
